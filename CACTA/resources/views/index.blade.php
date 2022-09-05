3882995c38e9c4ed4cf168045c8c43e2eb320eba
alexmunoz05 <71845532+alexmunoz05@users.noreply.github.com>
2022-09-05 15:03:47 -0600

    Login - Diseño

---
+++ CACTA/resources/views/index.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('/bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('/public/style.css')}}">
+
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #512F80";>
+        <a class="navbar-brand" href="#"  ><img src="images/logo.PNG" style="height: 50px" alt=""></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav ms-auto">
+            <a class="nav-item nav-link" href="#" style="color: wheat">Features</a>
+            <a class="nav-item nav-link " href="#" style="color: wheat">Pricing</a>
+          </div>
+        </div>
+      </nav>
+</head>
+<body>
+    <section class="section" style="padding-top: 80px">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 hidden-sm hidden-xs">
+                    <div class="custom-module" style="margin-right: 50px">
+                        <img   alt=""   style="visibility: visible; display: block;" src="images/ickkck.PNG">
+                    </div><!-- end module -->
+                </div><!-- end col -->
+                <div class="col-md-6">
+                    <div class="custom-module p40l">
+                        <h2 >BIENVENIDOS A CACTA</h2>
+
+                        <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dui felis, luctus et velit vitae, ullamcorper varius purus. 
+                            Maecenas a quam ac magna vulputate fermentum. Aenean fermentum nibh ut ante vehicula, et luctus leo bibendum. Sed aliquet nunc consequat imperdiet cursus. 
+                            Vivamus placerat luctus felis et vehicula. Morbi nec tempor justo. Curabitur vel massa accumsan, venenatis tortor at, vulputate sapien. Donec consectetur neque urna, vitae placerat velit hendrerit a. 
+                            Vivamus augue lorem, ultricies ac dolor id, commodo pulvinar ligula. Nam vel eleifend lacus. In consequat, tortor eget volutpat dignissim, tellus elit aliquam mauris, vitae vehicula risus orci ut eros. Donec auctor egestas varius. 
+                            Sed vitae turpis ante. Donec imperdiet eleifend mauris dapibus feugiat.</p>
+
+                        <hr class="invis">
+                        
+                        
+
+                        <hr class="invis">
+
+
+
+                    </div><!-- end module -->
+                </div><!-- end col -->
+            </div><!-- end row -->
+        </div><!-- end container -->
+    </section>
+
+
+    <script src="{{asset('/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+</body>
+</html>
